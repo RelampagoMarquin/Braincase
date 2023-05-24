19432581b0da314040016632b6f4384ad67fe040
devandresilva <andresilva.kat@gmail.com>
2023-05-23 22:49:26 -0300

    Estilização da tela de login

--- Web/src/views/Login.vue
+++ Web/src/views/Login.vue
@@ -18,44 +18,102 @@ const password = ref("");
 
 <!-- Parte de Estrutura: Template, o "html" -->
 <template>
-  <v-container class="align-center">
-    <v-row class="justify-center">
-      <v-col cols="10" md="4">
-        <h2 class="text-primary-custom mb-5 text-center">Login</h2>
-        <v-form>
-          <label for="email" class="text-label">E-mail</label>
-          <v-text-field
-            v-model="email"
-            placeholder="username@gmail.com"
-            density="compact"
-            variant="outlined"
-            class="mb-2"
-          ></v-text-field>
-          <label for="password" class="text-label">Senha</label>
-          <v-text-field
-            v-model="password"
-            placeholder="********"
-            type="password"
-            density="compact"
-            variant="outlined"
-            class="mb-2 v-text-field"
-          ></v-text-field>
-          <v-btn class="btn-primary" block outlined rounded="lg">
-            Entrar
-          </v-btn>
-        </v-form>
-        <p class="text-center mt-4">
-          Não possui um cadastro?
-          <router-link to="/signup">
-            Crie uma nova conta
-          </router-link>
-        </p>
+  <v-row class="container">
+      <!-- Left side login -->
+      <v-col cols="6">
+        <v-row class="login-container justify-center align-center">
+          <v-col cols="6">
+            <h2 class="text-primary-custom mb-5 text-center title">Login</h2>
+            <v-form>
+              <v-text-field
+                v-model="mail"
+                clearable
+                color="#F7A156"
+                label="E-mail"
+                variant="outlined"
+                class="mb-2 text-input"
+              ></v-text-field>
+              <v-text-field
+                v-model="password"
+                clearable
+                color="#F7A156"
+                label="Senha"
+                type="password"
+                variant="outlined"
+                class="mb-2"
+              ></v-text-field>
+              <v-btn class="btn-primary" block outlined rounded="sm">
+                Entrar
+              </v-btn>
+            </v-form>
+            <p class="text-center mt-4 signup-text">
+              Não possui um cadastro?
+              <router-link to="/signup" class="signup-link">
+                Crie uma nova conta
+              </router-link>
+            </p>
+          </v-col>
+        </v-row>
       </v-col>
+      <!-- mid wave div -->
+      <v-col cols="2">
+        <div class="spacer wave"></div>
+      </v-col>
+      <!-- right side div -->
+      <v-col cols="4" class="right-side"></v-col>
     </v-row>
-  </v-container>
 </template>
 
 <!-- Parte de Estilo: Style (CSS), vale salientar que aqui temos o "scoped", o css colocado aqui só é usado no escopo do elemento atual (nesse arquivo), ou seja, tem como personalizar a página de maneira individual -->
 <style scoped>
+div.text-input.v-field.v-field--appended {
+  border-radius: 5px;
+}
 
+.v-row {
+  margin: 0;
+}
+
+.container {
+  height: 100vh;
+}
+
+.login-container {
+  height: 100%;
+}
+
+.wave {
+  background-image: url('../../wave-login-page.svg');
+  aspect-ratio: 450/1024;
+  width: 100%;
+  height: 100%;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+}
+
+.right-side {
+  background-color: #F7A156;
+}
+
+.v-col {
+  padding: 0;
+}
+
+.title {
+  color: #F7A156;
+}
+
+.signup-text, .signup-link {
+  color: #F7A156;
+}
+
+.signup-link {
+  text-decoration: none;
+  font-weight: bold;
+}
+
+.mdi-close-circle {
+  color: #F7A156 !important;
+}
 </style>
