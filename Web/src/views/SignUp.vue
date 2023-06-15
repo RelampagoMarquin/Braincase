a92c928675d5226058100607951af9991a2ca60b
andinhodantas <andersonnascimentodantas@gmail.com>
2023-06-14 21:00:42 -0300

    pagina de cadastro

--- Web/src/views/SignUp.vue
+++ Web/src/views/SignUp.vue
@@ -1,93 +1,168 @@
-<!-- Parte Lógica: Script -->
-<script setup lang="ts">
-//import { useUserStore } from "../stores/userStore";
-import { ref } from "vue";
-//const userStore = useUserStore();
 
-//com o v-model é possível alterar o valor de elemento com essas variáveis, e o contrário também ocorre, quando o usuário digitar no campo, o valor dele é guardado nesta variável
-const email = ref("");
-const name = ref("");
-const registration = ref("");
-const password = ref("");
-const checkPassword = ref("");
+<script setup lang="ts">
+
+import { ref } from 'vue'
+
+const email = ref('')
+const name = ref('')
+const registration = ref('')
+const password = ref('')
+const checkPassword = ref('')
 
 // function signup() {
+  
 //   const data = {
-//     nome: nome.value,
+//     nome: name.value,
 //     email: email.value,
-//     senha: senha.value,
+//     senha: password.value,
 //   };
-//   if (senha.value !== confirmarSenha.value)
-//     return alert("As senhas não conferem");
-//   else {
-//     userStore.createUser(data);
-//   }
 // }
 </script>
 
-<!-- Parte de Estrutura: Template, o "html" -->
-<template>
-  <!-- o v-container, v-row, v-col funcionam como o container, class row e class col do bootstrap respectivamente -->
-  <v-container class="align-center">
-    <v-row class="justify-center">
-      <!-- aqui por exemplo temos o cols que diz quantas colunas (de 12) esse elemento irá ocupar, o md seria igual col-md do boorstrap e assim por diante -->
-      <v-col cols="10" md="4">
-        <h2 class="mb-5 text-center">Cadastre-se</h2>
-        <!-- área de formulário, para ver mais sobre seus atribtuos, pode-se ver na documentação do vuetify -->
-        <v-form>
-          <label for="name" class="text-label">Nome</label>
-          <!-- o v-text-field seria um campo de input e da alterar vários atributos dele, que podem ser vistos na documentação -->
-          <v-text-field
-            v-model="name"
-            placeholder="Ex: John Doe"
-            density="compact"
-            variant="outlined"
-            class="mb-2"
-          ></v-text-field>
-          <label for="email" class="text-label">E-mail</label>
-          <v-text-field
-            v-model="email"
-            placeholder="username@gmail.com"
-            density="compact"
-            variant="outlined"
-            class="mb-2"
-          ></v-text-field>
-          <label for="password" class="text-label">Senha</label>
-          <v-text-field
-            v-model="password"
-            placeholder="********"
-            type="password"
-            density="compact"
-            variant="outlined"
-            class="mb-2 v-text-field"
-          ></v-text-field>
-          <label for="checkPassword" class="text-label">Confirmar Senha</label>
-          <v-text-field
-            v-model="checkPassword"
-            placeholder="********"
-            type="password"
-            density="compact"
-            variant="outlined"
-            class="mb-2 v-text-field"
-          ></v-text-field>
-          <!-- botão do vuetify, atributos podem ser vistos na documentação -->
-          <v-btn class="btn-primary" block outlined rounded="lg">
-            Cadastrar-se
-          </v-btn>
-        </v-form>
-        <p class="text-center mt-4">
-          Já possui um cadastro?
-          <!-- o router-link é uma ancora (a), o "to" é a rota que você irá, nesse caso o "/", página inicial, que nas rotas está configurada como a página de login -->
-          <router-link to="/">
-            Fazer Login
-          </router-link>
-        </p>
-      </v-col>
-    </v-row>
-  </v-container>
+<template> 
+  <v-row class="container">
+    <v-col cols="4" class="right-side hidden-sm-and-down"></v-col>
+    <v-img class="logo hidden-sm-and-down" aspect-ratio="16/9" cover src="/Logomarca.svg"></v-img>
+    <v-img  class="image-art hidden-sm-and-down" aspect-ratio="16/9" cover src="/image-art.svg" ></v-img>
+    <v-col cols="2" class="hidden-sm-and-down" >
+      <div class="spacer wave hidden-sm-and-down"></div>
+    </v-col>
+
+    <v-col cols="lg-6 md-12" 
+    >
+      <v-row class="cadastro-container justify-center align-center">
+        <v-col cols="6" >
+          <h2 class="text-primary-custom mb-6 text-center title">Cadastro</h2>
+          <v-form>
+            <v-text-field
+              v-model="name" 
+              clearable
+              color="#F7A156"
+              label="Nome"
+              variant="outlined"
+              class="mb-2 text-input v-locale--is-ltr"
+            ></v-text-field>
+            <v-text-field
+              v-model="email"
+              required
+              clearable
+              color="#F7A156"
+              label="E-mail"
+              variant="outlined"
+              class="mb-2 text-input v-locale--is-ltr"
+            ></v-text-field>
+            <v-text-field
+              v-model="registration"
+              clearable
+              color="#F7A156"
+              label="Matrícula"
+              variant="outlined"
+              class="mb-2 text-input v-locale--is-ltr"
+              
+            ></v-text-field>
+            <v-text-field
+              v-model="password"
+              type="password"
+              required
+              clearable
+              color="#F7A156"
+              label="Senha"
+              variant="outlined"
+              class="mb-2 text-input v-locale--is-ltr"
+            ></v-text-field>
+
+            <v-text-field
+              v-model="checkPassword"
+              type="password"
+              clearable
+              required
+              color="#F7A156"
+              label="Confirmar senha"
+              variant="outlined"
+              class="mb-2 text-input v-locale--is-ltr"
+            ></v-text-field>
+            <v-btn class="btn-primary" @click="signup()"  block outlined rounded="sm"> Cadastrar </v-btn>
+          </v-form>
+          <p class="text-center mt-4 login-text">
+            Já possui um cadastro?
+            <router-link to="/" class="login-link"> Fazer Login </router-link>
+          </p>
+        </v-col>
+      </v-row>
+    </v-col>
+  </v-row>
 </template>
 
-<!-- Parte de Estilo: Style (CSS), vale salientar que aqui temos o "scoped", o css colocado aqui só é usado no escopo do elemento atual (nesse arquivo), ou seja, tem como personalizar a página de maneira individual -->
-<style scoped>
 
+<style scoped>
+.image-art  {
+  position: absolute;
+  left: 50px;
+  top: 180px;
+  width: 30%;
+}
+.logo{
+  position: absolute;
+  left: 180px;
+  top: 130px;
+  width: 12%;
+
+}
+
+.v-row {
+  margin: 0;
+}
+
+.container {
+  height: 100vh;
+}
+ 
+.cadastro-container {
+  height: 100vh;
+}
+
+.wave {
+  background-image: url('../../wave-login-page.svg');
+  aspect-ratio: 450/1024;
+  width: 100%;
+  height: 100%;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  transform: rotate(180deg);
+}
+
+.right-side {
+  background-color: #f7a156;
+}
+
+.v-col {
+  padding: 0;
+  
+}
+
+.title {
+  color: #f7a156;
+}
+
+.login-text,
+.login-link {
+  color: #f7a156;
+}
+
+.login-link {
+  text-decoration: none;
+  font-weight: bold;
+}
+
+.mdi-close-circle {
+  color: #f7a156 !important;
+}
+
+@media (max-width: 1000px){
+  .v-form{
+    margin:0;
+    
+  }
+}
 </style>
